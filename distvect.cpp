93ca5651e9a609e47b00f57e4c834e002b15bba6
Amol Bhagwat <arbhagwa@indiana.edu>
2015-11-27 03:09:20 -0500

    Rearranged code

--- distvect.cpp
+++ distvect.cpp
@@ -19,6 +19,30 @@ using 	namespace std;
 #define MAX_NODES 100
 #define BUFFER_SIZE 1472
 
+
+void initialize();
+void generateGraph();
+void generateRoutingTable();
+void *update(void*);
+void decrementTTLs();
+void sendAdv();
+void *getAdvs(void*);
+void processAdv(char *, char*);
+
+int hostnameToIp(const char*, sockaddr_in*);
+void refreshValues(string, string, char*);
+void readConfigFile();
+void displayRoutingTable();
+string makeAdv();
+void showStats();
+
+char configfilename[15];	
+	
+int portno, infinity;
+u_short ttl;
+u_short period;
+int 	shflag = 0; 						// split horizon flag
+
 string 	nodes[MAX_NODES]; 					// stores names of all the nodes, including itself
 string 	neighbours[MAX_NODES]; 				// stores names of self's neigbhours
 
@@ -28,63 +52,19 @@ int		graph[MAX_NODES][MAX_NODES];
 int 	node_count = 0; 					// total number of nodes in the network, including itself
 int 	neighbour_count = 0; 				// number of neigbhours
 
-int 	shflag = 0; 						// split horizon flag
-
 struct 	RouteEntry {
 	struct sockaddr_in destadr;  			// address of the destination
 	struct sockaddr_in nexthop;  			// address of the next hop
 	int cost;  								// distance
 	u_short ttl;  							// TTL in seconds
-}rtable[MAX_NODES];
-
-
-void initialize();
-void readConfigFile();
-void generateGraph();
-void displayGraph();
-void generateRoutingTable();
-void displayRoutingTable();
-void sendAdv();
-void *update(void*);
-void *getAdvs(void*);
-void decrementTTLs();
-string makeAdv();
-void processAdv(char *, char*);
-
-int hostnameToIp(const char*, sockaddr_in*);
-
-void refreshValues(string, string, char*);
-
-char configfilename[15];	
+} rtable[MAX_NODES];
 	
-int portno, infinity;
-u_short ttl;
-u_short period;
-	
-void showStats(){
-	// Just for printing
-	int i = 0;
-	cout << "Nodes: \n";
-	for (i=0; i< node_count; i++){
-		cout << i << " " << nodes[i] << " " << endl;
-	}
-	cout << "Node count: " << node_count << endl;
-	
-
-	cout << "\nNeighbours: \n";
-	for (i=0; i<neighbour_count; i++){
-		cout << neighbours[i] << " " << endl;
-	}
-
-	cout << "Neighbour count: " << neighbour_count << endl;
-
-}
 
 int main(int argc, char* argv[]) {
 	// check the parameters	
 	if(argc < 7){
 		cout << "Parameters: " << "Configfile Portnumber TTL Infinity Period Splithorizon" << endl;
-		return 1;	// no parameters passed
+		return 1;
 	}
 	
 	bzero((char *) &configfilename, sizeof(configfilename));
@@ -96,10 +76,10 @@ int main(int argc, char* argv[]) {
 	period = atoi(argv[5]);
 	shflag = atoi(argv[6]);
 
-	initialize();
-	
 	showStats();
-		
+	
+	initialize();
+			
 	pthread_t thread_update, thread_listener;
 
 	if( pthread_create( &thread_update , NULL , update , NULL) < 0){
@@ -194,11 +174,15 @@ void displayGraph(){
 }
 
 void generateRoutingTable(){
+	
 	// for own entry
+	
 	hostnameToIp(nodes[0].c_str(), &rtable[0].destadr);
 	hostnameToIp(nodes[0].c_str(), &rtable[0].nexthop);
 	rtable[0].cost = 0;
 	rtable[0].ttl = ttl;
+
+	// for the other nodes
 
 	for(int i = 1; i < node_count; i++){
 		// the destination node
@@ -239,10 +223,8 @@ void *update(void* a){
         decrementTTLs();
         sendAdv();          
     }
-
     return NULL;
 }
-
 
 void decrementTTLs(){
 	for(int i = 1; i < node_count; i++){
@@ -296,10 +278,7 @@ void *getAdvs(void *b) {
         inet_ntop(AF_INET, &client_address.sin_addr, from_ip, INET_ADDRSTRLEN);// << endl;
 		
 		processAdv(buffer, from_ip);
-
 	}
-
-
 	return NULL;
 }
 
@@ -326,8 +305,7 @@ void processAdv(char* recdadv, char* from_ip){
 
     // check for lower costs, and update/refresh if needed
 
-    char token1[20];
-	char token2[20];
+    char token1[20], token2[20];
 	char *temp_str;
 
 	temp_str = strtok (recdadv,",:");
@@ -353,7 +331,6 @@ void processAdv(char* recdadv, char* from_ip){
             rtable[i].ttl = ttl;
         }
     }
-
 }
 
 void refreshValues(string destination, string costtodestination, char *through){
@@ -389,9 +366,8 @@ void refreshValues(string destination, string costtodestination, char *through){
     }
 }
 
-
 int hostnameToIp(const char * hostname , sockaddr_in* node){
-	// http://stackoverflow.com/a/5445610
+	// Source: http://stackoverflow.com/a/5445610
 
 	struct hostent        *he;
 	
@@ -431,7 +407,7 @@ void sendAdv(){
 
     udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (udp_socket < 0) {
-       std::cout << "Error opening socket\n";
+       std::cout << "Error opening socket!\n";
        return;
     }
     
@@ -442,7 +418,7 @@ void sendAdv(){
             //cout << "Sending to: " << nodes[i] << " ";
 
             if (server == NULL) {
-               cout << nodes[i] << " FAILED - Host not found!" << endl;
+               cout << nodes[i] << "FAILED - Host not found - " << nodes[i] << endl;
                continue;
             }
             
@@ -452,16 +428,34 @@ void sendAdv(){
                 
             server_address.sin_port = htons(portno);
             if (connect(udp_socket,(struct sockaddr *) &server_address,sizeof(server_address)) < 0) {
-               std::cout << " FAILED - Error while connecting!" << endl;
+               std::cout << "FAILED - Error while connecting to " << nodes[i] << endl;
                continue;
             }
                 
             no_of_bytes = sendto(udp_socket,adv.c_str(),adv.length(),0,(struct sockaddr *)&server_address,sizeof(server_address));
             //cout << " SUCCESSFUL - Sent " << no_of_bytes << " Bytes" << endl;
             if (no_of_bytes < 0) {
-               std::cout << " FAILED - Error while writing!" << endl;
+               std::cout << "FAILED - Error while writing to " << nodes[i] << endl;
                continue;
             }
         }
     }
 }
+
+void showStats(){
+	int i = 0;
+	cout << "\nNodes: \n";
+	for (i=0; i< node_count; i++){
+		cout << i << " " << nodes[i] << " " << endl;
+	}
+	cout << "Node count: " << node_count << endl;
+	
+
+	cout << "\nNeighbours: \n";
+	for (i=0; i<neighbour_count; i++){
+		cout << neighbours[i] << " " << endl;
+	}
+
+	cout << "Neighbour count: " << neighbour_count << endl;
+}
+
